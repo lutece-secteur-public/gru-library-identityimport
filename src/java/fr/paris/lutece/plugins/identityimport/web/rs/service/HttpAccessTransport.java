2b264f256b932dcd9ced0fe819b5927b97e083c1
Loris Boiteux <lboiteux@yupiik.com>
2023-09-18 12:30:59 +0200

    LUT-24948 - #326 [ API suspicious ] la pagination n'est pas opérationnelle

--- src/java/fr/paris/lutece/plugins/identityimport/web/rs/service/HttpAccessTransport.java
+++ src/java/fr/paris/lutece/plugins/identityimport/web/rs/service/HttpAccessTransport.java
@@ -38,7 +38,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.paris.lutece.plugins.identityimport.web.service.CustomResponseStatusValidator;
 import fr.paris.lutece.plugins.identityimport.web.service.IHttpTransportProvider;
-import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.ResponseDto;
+import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.common.ResponseDto;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.error.ErrorResponse;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.util.Constants;
 import fr.paris.lutece.plugins.identitystore.web.exception.IdentityNotFoundException;
