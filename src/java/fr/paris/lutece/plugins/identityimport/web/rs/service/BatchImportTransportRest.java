e6b3b224134edfcefa5465d079547db14597965b
Loris Boiteux <lboiteux@yupiik.com>
2023-09-29 11:05:35 +0200

    LUT-24948 - missing headers

--- src/java/fr/paris/lutece/plugins/identityimport/web/rs/service/BatchImportTransportRest.java
+++ src/java/fr/paris/lutece/plugins/identityimport/web/rs/service/BatchImportTransportRest.java
@@ -89,11 +89,11 @@ public class BatchImportTransportRest extends AbstractTransportRest implements I
 
         final Map<String, String> mapHeadersRequest = new HashMap<>( );
         mapHeadersRequest.put( Constants.PARAM_CLIENT_CODE, strClientCode );
+        mapHeadersRequest.put( Constants.PARAM_AUTHOR_NAME, author.getName() );
+        mapHeadersRequest.put( Constants.PARAM_AUTHOR_TYPE, author.getType().name() );
         final Map<String, String> mapParams = new HashMap<>( );
 
-        final BatchImportResponse response = _httpTransport.doPostJSON( _strIdentityStoreImportEndPoint + Constants.VERSION_PATH_V3 + Constants.BATCH_PATH,
-                mapParams, mapHeadersRequest, request, BatchImportResponse.class, _mapper );
-
-        return response;
+        return _httpTransport.doPostJSON( _strIdentityStoreImportEndPoint + Constants.VERSION_PATH_V3 + Constants.BATCH_PATH, mapParams, mapHeadersRequest,
+                request, BatchImportResponse.class, _mapper );
     }
 }
